2174f3c4587013edf3851680e46768b8f6e747f2
Vincent <vincent.renard@gmx.com>
2018-11-01 12:43:43 +0100

    Main de test
    Signed-off-by: Vincent <vincent.renard@gmx.com>

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,42 @@ import graphe.Sequence2destructrice;
 
 public class Main {
     public static void main(String... args) {
+        System.out.println("Création des graphes");
 
         Graphe grapheA = new Graphe("res/grapheA.txt");
+        System.out.println("Graphe A");
+        grapheA.print();
+        System.out.println();
+
         Graphe grapheB = new Graphe("res/grapheB.txt");
+        System.out.println("Graphe B");
+        grapheB.print();
+        System.out.println();
+
+        System.out.println("Création des sequences");
         Sequence2destructrice s = new Sequence2destructrice("v1 v6 v3 v8 v2 v5 v4 v7");
         Sequence2destructrice s1 = new Sequence2destructrice("v2 v6 v1 v5 v4 v3 v7 v8");
+        System.out.println();
 
-       /* System.out.println(grapheA.isSeq2destr(s));
+
+        System.out.println("Tests des séquences");
+
+        System.out.println(grapheA.isSeq2destr(s));
         System.out.println(grapheB.isSeq2destr(s1));
+        System.out.println();
+        System.out.println("Tests des séquences fausses");
 
-        System.err.println(grapheA.isSeq2destr(s1));
-        System.err.println(grapheB.isSeq2destr(s));*/
+        System.out.println(grapheA.isSeq2destr(s1));
+        System.out.println(grapheB.isSeq2destr(s));
+        System.out.println();
 
-        //System.out.println(grapheA.generateSequence());
+
+        System.out.println("Génération d'une sequence");
         Sequence2destructrice seqt = grapheA.generateSequence();
-
-        System.err.println(grapheA.isSeq2destr(seqt));
+        Sequence2destructrice seqtb = grapheB.generateSequence();
+        System.out.println();
+        System.out.println("test des sequence");
+        System.out.println(grapheA.isSeq2destr(seqt));
+        System.out.println(grapheB.isSeq2destr(seqtb));
     }
 }
