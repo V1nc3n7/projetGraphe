778a4542bb1f52bf13ab01d92156503dc6451447
Lanes6 <guillaume.aguenier@laposte.net>
2018-11-07 15:58:01 +0100

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ import java.text.DecimalFormat;
 
 public class Main {
     private static final int NSUFFISEMENTGRAND = 1000;
+    private static final double TARGET = 0.5;
 
     public static Double testA(int nSommets, double probablite, double rougirSommets) {
         float experiences, probants;
@@ -24,13 +25,58 @@ public class Main {
         return Double.valueOf((new DecimalFormat("##.##").format((probants / experiences) * 100)));
     }
 
-    public static Double testB(int nSommets, double probablite) {
+    private static double repeatRandom(int nSommets, double probablite, double rougirSommets, int nb) {
+        double expriences, probants;
+        expriences = probants = 0;
+        while (expriences != nb) {
+            expriences++;
+            Graphe graphe = new Graphe(nSommets, probablite);
+            graphe.colorateGraphe(rougirSommets);
+            if (graphe.isSquencePossible()) {
+                probants++;
+            }
+        }
+        return (probants / expriences);
+    }
 
-        return 0.0;
+    public static Double testB(int nSommets, double probablite) {
+        DecimalFormat format= new DecimalFormat("#.##");
+        int iteration=1;
+        double borne_bas=0;
+        double borne_haut=1;
+        double milieu=borne_haut/2;
+        double res_borne_bas = repeatRandom(nSommets,probablite,borne_bas,NSUFFISEMENTGRAND);
+        double res_milieu = repeatRandom(nSommets,probablite,milieu,NSUFFISEMENTGRAND);
+
+        System.out.println("Itération: "+ iteration);
+        System.out.println("Borne: "+format.format(borne_bas)+"--"+format.format(milieu)+"--"+format.format(borne_haut));
+
+        //parcours dichotomique
+        while(!format.format(borne_bas).equals(format.format(milieu)) || !format.format(borne_haut).equals(format.format(milieu))) {
+            if((TARGET>=res_borne_bas && TARGET<res_milieu) || (TARGET<=res_borne_bas && TARGET>res_milieu)){
+                System.out.println(res_borne_bas+"-->TARGET<--"+res_milieu+"--res_haut");
+                borne_haut=milieu;
+                milieu=borne_bas+(milieu-borne_bas)/2;
+            }else{
+                System.out.println(res_borne_bas+"--"+res_milieu+"-->TARGET<--res_haut");
+                borne_bas=milieu;
+                milieu=milieu+(borne_haut-milieu)/2;
+            }
+            iteration++;
+            res_borne_bas = repeatRandom(nSommets,probablite,borne_bas,NSUFFISEMENTGRAND);
+            res_milieu = repeatRandom(nSommets,probablite,milieu,NSUFFISEMENTGRAND);
+            System.out.println("Itération: "+ iteration);
+            System.out.println("Borne: "+format.format(borne_bas)+"--"+format.format(milieu)+"--"+format.format(borne_haut));
+        }
+        String res=format.format(milieu);
+        res=res.replace(",",".");
+        System.out.println("Résultat: "+res+" en "+iteration+" itérations");
+        return Double.valueOf(res);
     }
 
     public static void main(String... args) {
-        System.out.println(testA(50, 1.0, 0.8));
+        System.out.println(testA(50, 0.5, 0.5));
+        System.out.println(testB(50, 0.1));
 
     }
 }
