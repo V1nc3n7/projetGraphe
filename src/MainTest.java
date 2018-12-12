403f79abdbb00e03fbb5c4be08d0a0df85a180c0
Viren <vincent.renard@gmx.com>
2018-12-12 18:30:00 +0100

    Final Signed-off-by: Viren <vincent.renard@gmx.com>
    Signed-off-by: Viren <vincent.renard@gmx.com>

--- src/MainTest.java
+++ src/MainTest.java
@@ -11,12 +11,14 @@ public class MainTest {
 
 
         System.out.println("Création des graphes");
-
+        System.out.println();
+        System.out.println("res/grapheA.txt");
         Graphe grapheA = new Graphe("res/grapheA.txt");
         System.out.println("Graphe A");
         grapheA.print();
         System.out.println();
 
+        System.out.println("res/grapheB.txt");
         Graphe grapheB = new Graphe("res/grapheB.txt");
         System.out.println("Graphe B");
         grapheB.print();
@@ -28,27 +30,37 @@ public class MainTest {
         System.out.println();
 
 
-        System.out.println("Tests des séquences");
+        System.out.println("Test des séquences");
 
+        System.out.println("Séquence 1 detruit Ga ? ");
         System.out.println(grapheA.isSeq2destr(s));
+        System.out.println("Séquence 2 detruit Gb ? ");
         System.out.println(grapheB.isSeq2destr(s1));
         System.out.println();
         System.out.println("Tests des séquences fausses");
-
+        System.out.println();
+        System.out.println("Séquence 1 detruit Ga ? ");
         System.out.println(grapheA.isSeq2destr(s1));
+        System.out.println("Séquence 2 detruit Gb ? ");
         System.out.println(grapheB.isSeq2destr(s));
         System.out.println();
 
         System.out.println("Génération d'une sequence");
+        System.out.print("Séquence 1 ");
         Sequence2destructrice seqt = grapheA.generateSequence();
+        System.out.println(seqt.toString());
         Sequence2destructrice seqtb = grapheB.generateSequence();
+        System.out.print("Séquence 2 ");
+        System.out.println(seqtb.toString());
         System.out.println();
-        System.out.println("Test des sequence");
+        System.out.println("Test des séquences générées");
+        System.out.println("Séquence 1 detruit Ga ? ");
         System.out.println(grapheA.isSeq2destr(seqt));
+        System.out.println("Séquence 2 detruit Gb ? ");
         System.out.println(grapheB.isSeq2destr(seqtb));
         System.out.println();
 
-        System.out.println("Génération aléatoire");
+        System.out.println("Génération aléatoire n: 30 ,p: 3/5,r: 1/5 ");
         Graphe testG = new Graphe(30, 0.6, 0.2);
         testG.print();
 
